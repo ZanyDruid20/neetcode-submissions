957437c180016839660758dda512217839383d8c
Furnom Dam <164426385+ZanyDruid20@users.noreply.github.com>
2026-07-29 22:14:44 -0400

    Add: daily-temperatures - submission-4

---
+++ Data Structures & Algorithms/daily-temperatures/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+
+        for index, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                prev_index = stack.pop()
+                answer[prev_index] = index - prev_index
+            stack.append(index)
+        return answer
